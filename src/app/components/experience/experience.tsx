cd823244f7b7c49498042d2c4c63bc797363ef22
Fernanda Kipper <nanda.kipper@gmail.com>
2023-06-12 10:21:02 -0300

    feat: add skills section

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,7 +1,7 @@
+import Image from "next/image"
 import { SectionTitle } from "../sectionTitle/section-title"
 
 import "./experience.scss"
-
 
 export function Experience(){
     return(
@@ -9,7 +9,62 @@ export function Experience(){
           <SectionTitle text="Experience"/>
           <p>3 years working as a Software Developer, in companys such as Itaú, Stone and Aftersale.</p>
           <div className="experience-time">
-
+            <div className="experience-language">
+              <Image
+              src="/react.png"
+                alt="React Logo"
+                width={40}
+                height={40}
+                priority
+              />
+              <div className="experience-unit">
+                <div className="experience-measure measure-2">
+                  <span>2 years</span>
+                </div>
+              </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/ts.png"
+                alt="Typescript Logo"
+                width={40}
+                height={40}
+                priority
+              />
+              <div className="experience-unit">
+                <div className="experience-measure measure-3">
+                  <span>3 years</span>
+                </div>
+              </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/js.png"
+                alt="Javascript Logo"
+                width={40}
+                height={40}
+                priority
+              />
+            <div className="experience-unit">
+              <div className="experience-measure measure-3">
+                <span>3 years</span>
+              </div>
+            </div>
+            </div>
+            <div className="experience-language">
+              <Image
+              src="/java.png"
+                alt="Java Logo"
+                width={40}
+                height={40}
+                priority
+              />
+              <div className="experience-unit">
+                <div className="experience-measure measure-1">
+                  <span>1 year</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
     )
